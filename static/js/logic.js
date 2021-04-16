734a6d3ec900fabe58f3f76a7255e8195f6c50ba
Esther Kim <74941139+estherkim0998@users.noreply.github.com>
2021-04-15 22:25:40 -0700

    worked on code

---
+++ static/js/logic.js
@@ -0,0 +1,22 @@
+console.log("working");
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+    attribution: 'Map data', 
+    maxZoom: 18, 
+    accessToken: API_KEY
+
+
+})
+
+let map = L.map('mapid', {
+    center: [40.7, -94.5],
+    zoom: 3,
+    layers: [streets]
+})
+
+let baseMap = { 
+    "Streets": streets,
+    // "Satellite": satelliteStreets,
+    //Light: light
+}
+
+//L.control.laters(baseMap. )
